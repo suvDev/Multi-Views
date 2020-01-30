173e1169885555ea4ea3523e6c1b6bb99e12a7c0
Suvansh Bansal <suvdev.work@gmail.com>
2020-01-30 22:40:49 +0530

    sample included

--- app/src/main/java/com/suvdev/multiviews/MainActivity.kt
+++ app/src/main/java/com/suvdev/multiviews/MainActivity.kt
@@ -2,12 +2,15 @@ package com.suvdev.multiviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val tv = group.getChildAt(0) as TextView
+        tv.setText("jxjsxjsxj")
     }
 }
